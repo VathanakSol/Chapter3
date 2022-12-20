e2bcd7db41b818355f4b6b2d0f5a94244f096747
Kim Uy Computer <Kim Uy Computer@VATHANAK>
2022-12-20 19:23:27 +0700

    Add project files.

---
+++ Chapter3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double tax, salary, bonus;
+            int spouse, children;
+            Console.Write("Enter Salary: "); salary = double.Parse(Console.ReadLine());
+            Console.Write("Enter Bonus: "); bonus = double.Parse(Console.ReadLine());
+            Console.Write("Enter Spouse: "); spouse = int.Parse(Console.ReadLine());
+            Console.Write("Enter Children: "); children = int.Parse(Console.ReadLine());
+
+            if (salary >= 0)
+            {
+                if (spouse >= 1)
+                {
+                    spouse = 1;
+                }
+                else
+                {
+                    spouse = 0;
+                }
+                if (children >= 9)
+                {
+                    children = 9;
+                }
+                double family = 150000 * (spouse + children);
+                tax = salary - family;
+
+                if (salary <= 1200000)
+                {
+                    tax = 0;
+                }
+
+                else if (salary <= 2000000)
+                {
+                    tax = salary * 0.05 - 60000;
+                }
+                else if (salary <= 8500000)
+                {
+                    tax = salary * 0.1 - 160000;
+                }
+                else if (salary <= 12500000)
+                {
+                    tax = salary * 0.15 - 585000;
+                }
+                else
+                    tax = salary * 0.2 - 1210000;
+                double lastsalary;
+                tax = tax + (bonus * 0.2);
+                lastsalary = salary - tax;
+                Console.WriteLine("==========================================");
+                
+                Console.WriteLine($"Tax: {tax:#,##0.00 R}");
+                Console.WriteLine($"Total Salary: {lastsalary:#,##0.## R}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter3/SalaryWorker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    class SalaryWorker
+    {
+        static void Mainx(string[] args)
+        {
+            double time1, time2, totalTime, basicSalary = 50.00, bonusSalary, totalSalary, bonusRate1, bonusRate2;
+            Console.Write("Enter Total Time = "); totalTime = double.Parse(Console.ReadLine());
+            if(totalTime >= 100 && totalTime <= 120)
+            {
+                time1 = totalTime - 100;
+                //time2 = totalTime - 120;
+                Console.WriteLine($"Extra Time 1 = {time1}");
+                Console.WriteLine("Extra Time 2 = 0");
+                Console.WriteLine("Bonus Rate 1 =  $1.25/h");
+                Console.WriteLine("Bonus Rate 2 =  $1.50/h");
+                bonusSalary = (time1 * 1.25);
+                Console.WriteLine("Basic Salary =  $50.00");
+                Console.WriteLine($"Bonus salary = {bonusSalary}");
+                Console.WriteLine("===============================");
+                totalSalary = basicSalary + bonusSalary;
+                Console.WriteLine($"Total Salary = {totalSalary:c2}");
+
+            }
+            else if(totalTime > 120)
+            {
+                //time1 = totalTime - 100;
+                time2 = totalTime - 120;
+                Console.WriteLine("Extra Time 1 = 0");
+                Console.WriteLine($"Extra Time 2 = {time2}");
+                Console.WriteLine("Bonus Rate 1 =  $1.25/h");
+                Console.WriteLine("Bonus Rate 2 =  $1.50/h");
+                bonusSalary = (time2 * 1.50);
+                Console.WriteLine("Basic Salary =  $50.00");
+                Console.WriteLine($"Bonus salary = {bonusSalary}");
+                Console.WriteLine("===============================");
+                totalSalary = basicSalary + bonusSalary;
+                Console.WriteLine($"Total Salary = {totalSalary:c2}");
+
+            }
+            if(totalTime < 100 && totalTime > 0)
+            {
+
+                double time3 = 100 - totalTime;
+                bonusSalary = (totalTime * 0.50);
+                Console.WriteLine($"Your total Salary = {bonusSalary:c2}");
+            }
+            if(totalTime < 0)
+            {
+                Console.WriteLine("Invalid ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
